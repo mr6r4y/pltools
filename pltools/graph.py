487f92493f55a0de481c18e0faf0b9f69eb6914e
mr6r4y <mr6r4y@gmail.com>
2017-10-17 17:25:27 +0300

    Add/fix

--- pltools/graph.py
+++ pltools/graph.py
@@ -1,29 +1,42 @@
-import graphviz as gv
-import subprocess as sp
-import collections
 import tempfile
 import os
+
+import subprocess as sp
+import collections
+from cgi import escape
+
+
+def esc(s):
+    return escape(s).replace("\\", "\\\\").replace("\n","\\l")
 
 
 def struct_to_label(s):
     if isinstance(s, tuple):
         sub_id, body = s
         if isinstance(body, basestring):
-            return '<%s> %s' % (sub_id, body)
+            return '<%s> %s' % (sub_id, esc(body))
         elif isinstance(body, collections.Iterable):
             b = " | ".join([struct_to_label(i) for i in body])
             return '{<%s> %s | {%s}}' % (sub_id, sub_id, b)
     elif isinstance(s, basestring):
-        return '<%s> %s' % (s, s)
+        return '<%s> %s' % (esc(s), esc(s))
 
 
 def arrow(g, edges):
     for i in range(len(edges) - 1):
         if isinstance(edges[i + 1], basestring):
-            g.edge(edges[i], edges[i + 1])
+            g.edge(esc(edges[i]), esc(edges[i + 1]))
         elif isinstance(edges[i + 1], collections.Iterable):
             for j in edges[i + 1]:
-                g.edge(edges[i], j)
+                g.edge(esc(edges[i]), esc(j))
+
+
+def nodes(g, nodes):
+    for n in nodes:
+        if isinstance(n, basestring):
+            g.node(name=esc(n))
+        elif isinstance(n, tuple):
+            g.node(name=esc(n[0]), label=esc(n[1]))
 
 
 def xdot(g):
